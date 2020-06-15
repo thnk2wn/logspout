4aff4d01a3b4356476609c5e17b0134329153c6d
Geoff Hudik <geoff.hudik@captiveaire.com>
2020-06-15 12:31:10 -0400

    apk to apt-get for debian img

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 set -e
 
 echo "Adding packages"
-apk add -v --update go build-base git mercurial ca-certificates
+#apk add -v --update go build-base git mercurial ca-certificates
+apt-get install build-base git mercurial ca-certificates
 
 echo "Switching to src dir"
 cd /src
@@ -10,7 +11,8 @@ echo "Running Go build"
 go build -ldflags "-X main.Version=$1" -o /bin/logspout
 
 echo "Deleting packages"
-apk del go git mercurial build-base
+#apk del go git mercurial build-base
+apt-get remove git mercurial build-base
 
 echo "Cleaning up"
 rm -rf /root/go /var/cache/apk/*
