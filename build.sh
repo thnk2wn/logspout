7b00b5e0037aa7126b28f4f24290e21426c3a57f
Geoff Hudik <geoff.hudik@captiveaire.com>
2020-06-15 09:28:30 -0400

    build script debuging

--- build.sh
+++ build.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 set -e
-apk add --update go build-base git mercurial ca-certificates
+
+echo "Adding packages"
+apk add -v --update go build-base git mercurial ca-certificates
+
+echo "Switching to src dir"
 cd /src
+
+echo "Running Go build"
 go build -ldflags "-X main.Version=$1" -o /bin/logspout
+
+echo "Deleting packages"
 apk del go git mercurial build-base
+
+echo "Cleaning up"
 rm -rf /root/go /var/cache/apk/*
 
 # backwards compatibility
+echo "Docker symbolic link"
 ln -fs /tmp/docker.sock /var/run/docker.sock
